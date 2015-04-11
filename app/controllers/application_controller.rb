639c496b2018df8b817eee6cfc6c30e6adb86cd2
Gerard Cruz <gerardcruz@live.com>
2015-04-11 12:47:27 +0800

    uncomment secrets in gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :
+
+  def home
+  end
+
+  def renter
+  end
+
+  def owner
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :type
